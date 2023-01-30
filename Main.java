444d982b5b7cb1af3bd799744085a74747690741
ebarrera2019263 <ebarrera-2019263@kinal.edu.gt>
2023-01-30 14:39:33 -0600

    se agrego el metodo y la libreria para poder leer el archivo txt

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        File  file = new File('datos.txt');
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bReader = new BufferedReader(fileReader);
+
+        
+
         
     }
 }
