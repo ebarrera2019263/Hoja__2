e077ed6a577dff2627c6ebd5784fab5fe588f751
ebarrera2019263 <ebarrera-2019263@kinal.edu.gt>
2023-01-30 14:47:47 -0600

    se agregaron los metodos para la interfaz Icalculadora

---
+++ ICalculadora.java
@@ -0,0 +1,6 @@
+public interface ICalculadora {
+    float add ( float a, float b);
+    float subtraction (float a, float b);
+    float multiply (float a, float b);
+    float divition (float a, float b);
+}
