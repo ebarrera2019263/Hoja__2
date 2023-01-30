b46420e4f8471884b8ea00fbfd8ba730d3e1fc55
ebarrera2019263 <ebarrera-2019263@kinal.edu.gt>
2023-01-30 16:51:21 -0600

    se realizo metodos en la clase Stack

--- Stack.java
+++ Stack.java
@@ -1,3 +1,33 @@
-public class Stack<E> implements IStack {
+public class Stack<E> implements IStack<E>{
+
+    protected Arraylist<E> stack = new Arraylist();
     
+
+    public void push(E item){
+        this.stack.add(item);
+
+    }
+    
+    public E pop{
+        return this.stack.remove(stack.size() -1);
+
+    }
+
+    public E peek(){
+        return this.stack.get(stack.size() -1);
+    }
+    
+
+    public boolean empty(){
+        if(stack !=null){
+            return false;
+        }
+        return true;
+
+    }
+
+
+    public int size(){
+        return this.stack.size();
+    }
 }
