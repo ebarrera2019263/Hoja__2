b64d9e7164a1730cf24e526fcc8611888046e8a7
ebarrera2019263 <ebarrera-2019263@kinal.edu.gt>
2023-01-30 19:55:35 -0600

    se agrego la interface icalculadora finalizada

---
+++ ICalculadora.java
@@ -0,0 +1,10 @@
+public interface ICalculadora {
+
+    double sum(double a, double b);
+
+    double substraction(double a, double b);
+
+    double multiply(double a, double b);
+
+    double divition(double a, double b);
+}
