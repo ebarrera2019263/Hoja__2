b28b785af33d4226d12fd3c56ef9d8c64b4497c2
ebarrera2019263 <ebarrera-2019263@kinal.edu.gt>
2023-01-30 19:44:07 -0600

    se agrego la clase calculadora finalizada

--- Calculadora.java
+++ Calculadora.java
@@ -1,3 +1,25 @@
-public class Calculadora {
+public class Calculadora implements ICalculadora {
+
+    double resultado;
+
+    public double sum(double a, double b) {
+        this.resultado = a + b;
+        return this.resultado;
+    }
+
+    public double substraction(double a, double b) {
+        this.resultado = a - b;
+        return this.resultado;
+    }
+
+    public double multiply(double a, double b) {
+        this.resultado = a * b;
+        return this.resultado;
+    }
+
+    public double divition(double a, double b) {
+        this.resultado = a / b;
+        return this.resultado;
+    }
     
 }
